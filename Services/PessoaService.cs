5571d13a407f38ebd0bf6e5b5a12d613c413f4b7
danillo <danilo_victor2004@hotmail.com>
2026-05-10 10:51:25 -0300

    feature/validated-CPF

--- Services/PessoaService.cs
+++ Services/PessoaService.cs
@@ -40,5 +40,49 @@ namespace challange_by_coodesh.Services
             }
             File.WriteAllText(_filePath, json);
         }
+
+        public bool IsCpfValido(string cpf)
+        {
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if(cpf.Length != 11)
+            {
+                return false;
+            }
+
+            if (new string(cpf[0], 11) == cpf)
+            {
+                return false;
+            }
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            }
+
+            int resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+
+            string digito = resto.ToString();
+            tempCpf += digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
     }
 }
