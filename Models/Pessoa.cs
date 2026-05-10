e3004951396377572657636ac530881d4d4f297f
danillo <danilo_victor2004@hotmail.com>
2026-05-10 10:28:58 -0300

    feat: Add Person

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -6,5 +6,9 @@ namespace challange_by_coodesh.Models
 {
     class Pessoa
     {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
     }
 }
