1c9483d58946c9bf9f696c33d943a1a0f20256f3
danillo <danilo_victor2004@hotmail.com>
2026-05-11 09:20:02 -0300

    feature/Implements ProdutoServices

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -6,5 +6,9 @@ namespace challange_by_coodesh.Models
 {
     class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string CodigoProduto { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
     }
 }
---
+++ Services/ProdutoService.cs
@@ -0,0 +1,37 @@
+﻿using challange_by_coodesh.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace challange_by_coodesh.Services
+{
+    class ProdutoService
+    {
+        private readonly string _filePath = "Data/Produto.json";
+
+        public List<Produto> GetProdutos()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Produto>();
+            }
+            
+            var json = File.ReadAllText(_filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new List<Produto>();
+            }
+
+            return JsonSerializer.Deserialize<List<Produto>>(json) ?? new List<Produto>();
+        }
+
+        public void SaveProdutos(List<Produto> produtos)
+        {
+            var json = JsonSerializer.Serialize(produtos, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
